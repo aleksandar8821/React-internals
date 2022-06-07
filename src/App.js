c9848e2d4a89c6439fb60e78d4bea387cad56328
Aleksandar Foltin <aleksandar.foltin@gmail.com>
2022-06-07 21:05:23 +0200

    2. virtual dom 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+  const markup = <div>
+    Hello World!
+  </div>
+
+  console.log(markup)
+
+  return markup;
 }
 
 export default App;
+
+// const output = {
+//   $$typeof: Symbol(react.element),
+//   key: null,
+//   props: {
+//     children: "Hello World!"
+//   },
+//   ref: null,
+//   type: "div"
+// }
