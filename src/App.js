7d453704ae3bc859da453a0d1cc50a8217d1f48e
Aleksandar Foltin <aleksandar.foltin@gmail.com>
2022-06-09 07:54:41 +0200

    12. use effect 1 - basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import { Parent } from "./Parent";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <Parent />;
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+
+  useEffect(() => {
+    console.log("effect executed");
+  })
+
+  console.log("rendered");
+
+  return (
+    <>
+      <button type="button" onClick={increaseByOne}>
+        Click me!
+      </button>
+      <br />
+      <br />
+      You clicked {counter} times!
+    </>
+  );
 }
 
 export default App;
