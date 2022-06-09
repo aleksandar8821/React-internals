ff96c4b65c20ad088a21c723fb90082a7d159ed7
Aleksandar Foltin <Aleksandar.Foltin@netcetera.com>
2022-06-09 12:25:47 +0200

    17. use effect stale closure issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import { Parent } from "./Parent";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <Parent />;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      console.log("count value inside interval callback", count);
+      setCount(count + 1);
+    }, 2000);
+    return () => clearInterval(id);
+  }, []);
+
+  return <h1>{count}</h1>;
 }
 
 export default App;
