387b4825b310e58975d68e49422f902f0da4326c
Aleksandar Foltin <Aleksandar.Foltin@netcetera.com>
2022-06-09 11:13:10 +0200

    14. use effect cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import { Parent } from "./Parent";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <Parent />;
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+
+  useEffect(() => {
+    console.log("effect executed with counter value: ", counter);
+
+    return function cleanup() {
+      // Intro to stale closures
+      console.log("cleanup executed with counter value: ", counter);
+    };
+  });
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     console.log(Math.floor(Math.random() * 10));
+  //   }, 1000);
+
+  //   return function cleanup() {
+  //     clearInterval(interval);
+  //   };
+  // });
+
+  return (
+    <>
+      <button type="button" onClick={increaseByOne}>
+        Click me!
+      </button>
+      <br />
+      <br />
+      You clicked {counter} times!
+    </>
+  );
 }
 
 export default App;
